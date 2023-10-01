50db5a267d3133eec3650febe39a59ef6a3725b2
Ghaalia <Ghaliaals9@gmail.com>
2023-10-01 19:21:53 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import image from "./Bakery.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Ghaalia's Bakery</h1>
+      <p>Delicious delights baked with love, making every moment a sweet celebration</p>
+      <img src={image}/>
     </div>
   );
 }
