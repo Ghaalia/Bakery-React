3d817b37f8b818b546aaf923b3e6a4868e6edd1e
Ghaalia <Ghaliaals9@gmail.com>
2023-10-01 20:22:37 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-
-import './App.css';
-import image from "./Bakery.png"
+import "./App.css";
+import image from "./Bakery.jpg";
+import cupcake from "./CupCake.jpg";
+import cheeseCake from "./CheeseCake.jpeg";
 
 function App() {
+  const products = [
+    {
+      name: "CupCake",
+      photo: cupcake,
+      price: 1.75,
+    },
+    {
+      name: "CheeseCake",
+      photo: cheeseCake,
+      price: 2.25,
+    },
+  ];
+
+  const productList = products.map((product) => {
+    return (
+      <div className="card">
+        <h2>{product.name}</h2>
+        <img src={product.photo} className="imageSize" />
+        <h2>Price: {product.price} K.D.</h2>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
-     <h1>Ghaalia's Bakery</h1>
-      <p>Delicious delights baked with love, making every moment a sweet celebration</p>
-      <img src={image}/>
+      <h1>Ghaalia's Bakery</h1>
+      <p>
+        Delicious delights baked with love, making every moment a sweet
+        celebration
+      </p>
+      <img src={image} className="image" />
+
+      <div className="container">{productList}</div>
     </div>
   );
 }
